909ce218a378e2d8298afcc336954c98afe5703a
Vinh Le Duy <vinhduyle@Vinhs-MacBook-Pro.local>
2022-01-06 15:13:42 -0800

    first commit

---
+++ src/main/java/com/mySQL/cloudconnection/CloudconnectionApplication.java
@@ -0,0 +1,13 @@
+package com.mySQL.cloudconnection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudconnectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudconnectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mySQL/cloudconnection/CloudconnectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mySQL.cloudconnection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudconnectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
